70a6a145986f3b26a3f31ea755632c0c69cf17a5
kartik <kabakshi@hotmail.ca>
2021-10-25 19:57:50 -0400

    implementing assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(' ✅✅✅ ' + ' Assertion Passed: ' + actual + ' === ' + expected);
+
+  } else {
+    console.log('❌❌❌' + '   Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+
+};
+
+// TEST CODE
+console.log(assertEqual("This is an identical string", "This is an identical string"));
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+console.log(assertEqual(10, 30));
