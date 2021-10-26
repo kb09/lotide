555be7124431dfe0c17d0dd3aa3c93fb01eb8e44
kartik <kabakshi@hotmail.ca>
2021-10-25 20:40:50 -0400

    implement head

---
+++ head.js
@@ -0,0 +1,30 @@
+
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(` ✅✅✅  Assertion Passed: ${actual} ===  ${expected}`);
+
+  } else {
+    console.log(` ❌❌❌  Assertion Failed: ${actual}  !==  ${expected}`);
+  }
+
+};
+
+const head = function (array, index){
+  if (array === []) {
+    return undefined;
+  } else {
+  
+  
+   index = array[0];
+   return index;
+  }
+}
+// TEST CODE
+console.log(assertEqual(head([5,6,7]), 5));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+
+console.log(assertEqual(head([10]), 5));
+console.log(assertEqual(head([5]), 5));
+console.log(assertEqual(head([]), 5));
