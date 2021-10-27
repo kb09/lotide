048b4c9350afa94d37edbdba17a4e27183ce6121
kartik <kabakshi@hotmail.ca>
2021-10-27 19:05:16 -0400

    Only Count Specific Items

--- countOnly.js
+++ countOnly.js
@@ -1,22 +1,49 @@
-// allItems: an array of strings that we need to look through
-// itemsToCount: an object specifying what to count
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(` ✅✅✅  Assertion Passed: ${actual} ===  ${expected}`);
+
+  } else {
+    console.log(` ❌❌❌  Assertion Failed: ${actual}  !==  ${expected}`);
+  }
+
+};
+
+
+// countOnly
+
 const countOnly = function(allItems, itemsToCount) {
-  const firstNames = [
-    "Karl",
-    "Salima",
-    "Agouhanna",
-    "Fang",
-    "Kavith",
-    "Jason",
-    "Salima",
-    "Fang",
-    "Joe"
-  ];
+  const results = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
   
-  const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+  return results;
+};
+
   
-  assertEqual(result1["Jason"], 1);
-  assertEqual(result1["Karima"], undefined);
-  assertEqual(result1["Fang"], 2);
-  assertEqual(result1["Agouhanna"], undefined);
-}
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
