4fc747b239a512eb543106557b3dca7ee1ff2c75
kartik <kabakshi@hotmail.ca>
2021-10-27 18:43:43 -0400

    declaring the function

--- countOnly.js
+++ countOnly.js
@@ -1,24 +1,4 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(` ✅✅✅  Assertion Passed: ${actual} ===  ${expected}`);
-
-  } else {
-    console.log(` ❌❌❌  Assertion Failed: ${actual}  !==  ${expected}`);
-  }
-
-};
-
-// countOnly
-
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
-  result=[];
-  for ( let item in allItems){
-    if ( allItems[item].incldes(itemsToCount)){
-      return result.push(itemsToCount);
-    } else {
-      result[item];
-    }
-  }
-  return result;
 }
