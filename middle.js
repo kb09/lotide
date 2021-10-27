e761a18960a9b70788c20ee4c28fd9162d301daf
kartik <kabakshi@hotmail.ca>
2021-10-27 09:50:52 -0400

    add middle function

---
+++ middle.js
@@ -0,0 +1,50 @@
+
+/*
+// assertEqual
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(` ✅✅✅  Assertion Passed: ${actual} ===  ${expected}`);
+
+  } else {
+    console.log(` ❌❌❌  Assertion Failed: ${actual}  !==  ${expected}`);
+  }
+
+};
+*/
+
+//eqArrays
+
+const eqArrays = function (arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length){
+    return false;
+  } 
+  for ( let i in arrayOne) {
+    if ( arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+
+  } return true;
+  
+}
+
+// assertArraysEqual
+
+const assertArraysEqual = function (arrayOne, arrayTwo) {
+    if ( !eqArrays(arrayOne, arrayTwo)) {
+      console.log (' Arrays are not the same ')
+    } else {
+      console.log ( ' Arrays are the same');
+    }
+   }
+  assertArraysEqual([1,2,3],[1,2,3])
+  
+
+
+  // middle
+
+  const middle = function ( array){
+    if (array[0] || array[1]) {
+      return [];
+    }
+  }
