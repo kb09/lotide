4a35e3191676bf30d0489acac37fefe8a77e820d
kartik <kabakshi@hotmail.ca>
2021-10-26 21:42:55 -0400

    create asserArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,29 @@
+//eqArrays
+
+const eqArrays = function (arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length){
+    return false;
+  } 
+  for ( let i in arrayOne) {
+    if ( arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+
+  } return true;
+  
+}
+
+// assertEqual 
+
+const assertEqual = function (firstArray, secondArray) {
+  const result = eqArrays ( firstArray, secondArray) 
+    if ( result === false ) {
+      console.log (' false ')
+    } else {
+      console.log ( ' true ');
+    }
+  
+}
+
+// TEST CODE
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
