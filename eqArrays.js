a96aefb526ad229b1784386e1073fe6352b0076e
kartik <kabakshi@hotmail.ca>
2021-10-26 21:17:54 -0400

    add false return statement

--- eqArrays.js
+++ eqArrays.js
@@ -15,6 +15,12 @@ const eqArrays = function (arrayOne, arrayTwo) {
   if (arrayOne.length !== arrayTwo.length){
     return false;
   } //else{}
+  for ( let i = 0; i < arrayOne.length; i++) {
+    if ( arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+
+  } 
   
 }
 
