15812a6f20af1c33771406ba763a22b9ccc3287c
kartik <kabakshi@hotmail.ca>
2021-10-28 18:01:46 -0400

    Change the function to take in two arguments.

--- map.js
+++ map.js
@@ -1,4 +1,8 @@
-const map = function() {
+const map = function(array, callback) {
+  // temporary code:
+  console.log('array: ', array);
+  console.log('callback: ', callback);
+
   const results = [];
   return results;
 }
