78d6bb30d727c50fb4e1084792f87cf2af5c3636
kartik <kabakshi@hotmail.ca>
2021-10-27 20:40:57 -0400

    loop through sentence

---
+++ letterPositions.js
@@ -0,0 +1,39 @@
+
+// //assertArraysEqual
+
+// const assertArraysEqual = function (arrayOne, arrayTwo) {
+//     if ( !eqArrays(arrayOne, arrayTwo)) {
+//       console.log (' Arrays are not the same ')
+//     } else {
+//       console.log ( ' Arrays are the same');
+//     }
+//    }
+
+// //eqArrays
+
+// const eqArrays = function (arrayOne, arrayTwo) {
+//   if (arrayOne.length !== arrayTwo.length){
+//     return false;
+//   } 
+//   for ( let i in arrayOne) {
+//     if ( arrayOne[i] !== arrayTwo[i]) {
+//       return false;
+//     }
+
+//   } return true;
+  
+// }
+
+
+// letterPositions
+
+const letterPositions = function(sentence) {
+  const results = {};
+  // logic to update results here
+  for (let i = 0 ; i < sentence.length; i++){
+  }
+  return results;
+};
+
+console.log(letterPositions('hello'))
+console.log(assertArraysEqual(letterPositions("hello").e,[1]));
