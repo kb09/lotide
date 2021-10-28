6c62657a1aab91bf1a14cfb41a62bc46ef55a654
kartik <kabakshi@hotmail.ca>
2021-10-28 18:50:24 -0400

    Add the loop logic which just console.logs each item in the provided array as well as what happens to the item after being passed into the callback.

--- map.js
+++ map.js
@@ -1,4 +1,38 @@
+
+
+//eqArrays
+
+const eqArrays = function (arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length){
+    return false;
+  } 
+  for ( let i in arrayOne) {
+    if ( arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+
+  } return true;
+  
+}
+
+// assertArraysEqual
+
+const assertArraysEqual = function (arrayOne, arrayTwo) {
+    if ( !eqArrays(arrayOne, arrayTwo)) {
+      console.log (' Arrays are not the same ')
+    } else {
+      console.log ( ' Arrays are the same');
+    }
+   }
+  assertArraysEqual([1,2,3],[1,2,3])
+  
+  
+  
+//map
+
+
 const words = ["ground", "control", "to", "major", "tom"];
+
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
@@ -8,3 +42,8 @@ const map = function(array, callback) {
   }
   return results;
 }
+
+
+const results1 = map(words, word => word[0]);
+
+console.log(results1);
