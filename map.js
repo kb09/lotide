2c473795c734ebc8608f41664f4302e13eb55b4b
kartik <kabakshi@hotmail.ca>
2021-10-28 18:44:03 -0400

    Add the loop logic which just console.logs each item in the provided array as well as what happens to the item after being passed into the callback.

--- map.js
+++ map.js
@@ -1,8 +1,10 @@
+const words = ["ground", "control", "to", "major", "tom"];
 const map = function(array, callback) {
-  // temporary code:
-  console.log('array: ', array);
-  console.log('callback: ', callback);
-
   const results = [];
+  for (let item of array) {
+    console.log('item BEFORE: ', item);
+    console.log('item AFTER: ', callback(item));
+    console.log('---');
+  }
   return results;
 }
