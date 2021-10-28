dd9769c693e086cff9567dd5793a50c34093d868
kartik <kabakshi@hotmail.ca>
2021-10-28 19:53:56 -0400

    use assertArraysEqual to compare the results from map with expected array results.

--- map.js
+++ map.js
@@ -1,45 +1,44 @@
+// assertEqual
 
-// assertArraysEqual
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(` ✅✅✅  Assertion Passed: ${actual} ===  ${expected}`);
 
-const assertArraysEqual = function (arrayOne, arrayTwo) {
-  if ( !eqArrays(arrayOne, arrayTwo)) {
-    console.log (' Arrays are not the same ')
   } else {
-    console.log ( ' Arrays are the same');
+    console.log(` ❌❌❌  Assertion Failed: ${actual}  !==  ${expected}`);
   }
- }
+
+};
 
 
-  //eqArrays
+//eqArrays
 
 const eqArrays = function (arrayOne, arrayTwo) {
   if (arrayOne.length !== arrayTwo.length){
     return false;
   } 
   for ( let i in arrayOne) {
-    if( arrayOne[i] !== arrayTwo[i]) {
+    if ( arrayOne[i] !== arrayTwo[i]) {
       return false;
     }
 
   } return true;
   
 }
+
+// assertArraysEqual
+
+const assertArraysEqual = function (arrayOne, arrayTwo) {
+    if ( !eqArrays(arrayOne, arrayTwo)) {
+      console.log ( ` ❌❌❌  Assertion Failed: ${arrayOne}  !==  ${arrayTwo}` )
+    } else {
+      console.log ( ` ✅✅✅  Assertion Passed: ${arrayOne} ===  ${arrayTwo}`);
+    }
+   }
+  
+
   
 //map
-
-// const words = ["ground", "control", "to", "major", "tom"];
-
-// const map = function(array, callback) {
-//   const results = [];
-//   for (let item of array) {
-//     results.push(callback(item));
-//   }
-//   return results;
-// }
-
-// const results1 = words.map(function(word){
-//   return word[0];
-// } );
 
 const words = ["ground", "control", "to", "major", "tom"];
 
@@ -49,8 +48,14 @@ const map = function(array, callback) {
     results.push(callback(item));
   }
   return results;
-};
+}
 
-assertArraysEqual(map(words, word => word[0]), [ 'g', 'c', 't', 'm', 't' ]);
-assertArraysEqual(map(words, word => "x" + word + "x"), ['xgroundx', 'xcontrolx', 'xtox', 'xmajorx', 'xtomx']);
-assertArraysEqual(map(words, word => word.toUpperCase()), [ 'GROUND','CONTROL','TO','MAJOR','TOM' ]);
+const results1 = words.map(function(word){
+  return word[0];
+} );
+
+
+
+ assertArraysEqual(map(words, word => word[0]), [ 'g', 'c', 't', 'm', 't' ]);
+ assertArraysEqual(map(words, word => word.toUpperCase()), [ 'GROUND','CONTROL','TO','MAJOR','TOM' ]);
+ assertArraysEqual(map(words, word => word + 1), ['ground1', 'control1', 'to1', 'major1', 'tom1']);
