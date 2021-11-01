e9ac04f446b0c5222741b9f066154b1647e878df
kartik <kabakshi@hotmail.ca>
2021-11-01 19:25:45 -0400

    implement middle

--- middle.js
+++ middle.js
@@ -1,46 +1,3 @@
-
-/*
-// assertEqual
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(` ✅✅✅  Assertion Passed: ${actual} ===  ${expected}`);
-
-  } else {
-    console.log(` ❌❌❌  Assertion Failed: ${actual}  !==  ${expected}`);
-  }
-
-};
-*/
-
-//eqArrays
-
-const eqArrays = function (arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length){
-    return false;
-  } 
-  for ( let i in arrayOne) {
-    if ( arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-
-  } return true;
-  
-}
-
-// assertArraysEqual
-
-const assertArraysEqual = function (arrayOne, arrayTwo) {
-    if ( !eqArrays(arrayOne, arrayTwo)) {
-      console.log (' Arrays are not the same ')
-    } else {
-      console.log ( ' Arrays are the same');
-    }
-   }
-  assertArraysEqual([1,2,3],[1,2,3])
-  
-
-
   // middle
 
   const middle = function (array){
@@ -55,12 +12,4 @@ const assertArraysEqual = function (arrayOne, arrayTwo) {
    }
 
    
-   // Test code
-   
-console.log(middle([1])); // => []
- 
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+   module.exports = {middle}
