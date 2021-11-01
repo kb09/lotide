8b306f87a36f4762bda21d0d785011905e76179a
kartik <kabakshi@hotmail.ca>
2021-11-01 19:15:57 -0400

    implement assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,31 +1,4 @@
-
-// assertEqual
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(` ✅✅✅  Assertion Passed: ${actual} ===  ${expected}`);
-
-  } else {
-    console.log(` ❌❌❌  Assertion Failed: ${actual}  !==  ${expected}`);
-  }
-
-};
-
-
-//eqArrays
-
-const eqArrays = function (arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length){
-    return false;
-  } 
-  for ( let i in arrayOne) {
-    if ( arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-
-  } return true;
-  
-}
+const {eqArrays} = require("./eqArrays");
 
 // assertArraysEqual
 
@@ -36,8 +9,7 @@ const assertArraysEqual = function (arrayOne, arrayTwo) {
       console.log ( ` ✅✅✅  Assertion Passed: ${arrayOne} ===  ${arrayTwo}`);
     }
    }
-  assertArraysEqual([1,2,3],[1,2,3])
   
 
-
+module.exports = {assertArraysEqual};
   
