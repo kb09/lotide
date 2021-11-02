4df58e9cd84cc9dd9fddeb7fb4e6ef7e78c1091c
kartik <kabakshi@hotmail.ca>
2021-11-01 21:29:01 -0400

    middleTest with mocha and chai

--- test/middle.test.js
+++ test/middle.test.js
@@ -1,12 +1,35 @@
 const {middle} =require('../middle')
-const {assertArraysEqual} =require('../assertArraysEqual')
+const assert = require('chai').assert;
 
-// Test code
+// // Test code
    
-console.log(middle([1])); // => []
+// console.log(middle([1])); // => []
  
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
+// console.log(middle([1, 2, 3])); // => [2]
+// console.log(middle([1, 2, 3, 4, 5])); // => [3]
 
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+// console.log(middle([1, 2, 3, 4])) // => [2, 3]
+// console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+
+describe("#middle", () => {
+
+  it("returns [ 2 ] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2] ); // ask why it doesn't work if [2] is put as 2 without brackets
+  });
+
+  it("returns [ 8 ] for [1, 8, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2] ); // ask why it doesn't work if [2] is put as 2 without brackets
+  });
+
+  it("returns [ 2,3 ] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2] ); // ask why it doesn't work if [2] is put as 2 without brackets
+  });
+
+  it("returns [ 5,8 ] for [2, 5, 8, 8]", () => {
+    assert.deepEqual(middle([2, 5, 8, 8]), [5,8] ); // ask why it doesn't work if [2] is put as 2 without brackets
+  });
+
+}
+
+
+)
