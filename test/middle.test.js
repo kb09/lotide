04ecd3a87be14564d987bdc571711dace6e37ddd
kartik <kabakshi@hotmail.ca>
2021-11-01 21:30:19 -0400

    middleTest with mocha and chai

--- test/middle.test.js
+++ test/middle.test.js
@@ -1,15 +1,7 @@
 const {middle} =require('../middle')
 const assert = require('chai').assert;
 
-// // Test code
-   
-// console.log(middle([1])); // => []
- 
-// console.log(middle([1, 2, 3])); // => [2]
-// console.log(middle([1, 2, 3, 4, 5])); // => [3]
 
-// console.log(middle([1, 2, 3, 4])) // => [2, 3]
-// console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
 
 describe("#middle", () => {
 
